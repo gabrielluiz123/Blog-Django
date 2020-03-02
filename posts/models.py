812743debebee9f2f37d4a97b3ab76763eb3efa0
Unknown <gabriells760@gmail.com>
2020-03-02 20:35:35 -0300

    Images
    Correção da inserção de imagens, diminuindo o tamanho das imagens inseridas no servidor e tamanho das imagens na div do index

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categorias.models import Categoria
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 
 class Post(models.Model):
@@ -16,3 +19,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo_post
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image(self.imagem_post.name, 800)
+
+    @staticmethod
+    def resize_image(nome_imagem, nova_largura):
+        img_path = os.path.join(settings.MEDIA_ROOT, nome_imagem)
+        img = Image.open(img_path)
+        width, height = img.size
+        new_height = round((nova_largura*height) / width)
+
+        if width <= nova_largura:
+            img.close()
+            return
+        nova_img = img.resize((nova_largura, new_height), Image.ANTIALIAS)
+        nova_img.save(
+            img_path,
+            optimize=True,
+            quality=60
+        )
+        nova_img.close()
